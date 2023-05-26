7e6fe8c9231697e513fab6c41dfd48cc268369f1
Sergey Yerofeev <erofsey@gmail.com>
2023-05-26 11:14:48 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup="">
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>To-Do List</h1>
+  </div>
+</template>
+
+<style lang="scss">
+  @import './assets/main.scss';
+</style>
